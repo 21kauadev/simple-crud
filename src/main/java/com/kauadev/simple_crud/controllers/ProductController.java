9da717787052ca5ae7b69d4f9c2bab4f36b32682
Kaua2123 <kauantnetsoares@gmail.com>
2025-08-04 10:36:51 -0300

    chore: add clas ApiResponse and RestErrorMessage to best http return message with ResponseEntity

--- src/main/java/com/kauadev/simple_crud/controllers/ProductController.java
+++ src/main/java/com/kauadev/simple_crud/controllers/ProductController.java
@@ -3,12 +3,15 @@ package com.kauadev.simple_crud.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kauadev.simple_crud.domain.product.Product;
+import com.kauadev.simple_crud.infra.ApiResponse;
 import com.kauadev.simple_crud.services.ProductService;
 
 @RestController
@@ -19,13 +22,21 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/")
-    public List<Product> getAllProducts() {
-        return productService.getAllProducts();
+    public ResponseEntity<ApiResponse<List<Product>>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        ApiResponse<List<Product>> response = new ApiResponse<List<Product>>(HttpStatus.OK.value(), true,
+                "Produtos retornados: ",
+                products);
+
+        return ResponseEntity.ok().body(response);
     }
 
     @GetMapping("/{id}")
-    public Product getProduct(@PathVariable("id") Long id) {
-        return productService.getProduct(id);
+    public ResponseEntity<ApiResponse<Product>> getProduct(@PathVariable("id") Long id) {
+        Product product = productService.getProduct(id);
+        ApiResponse<Product> response = new ApiResponse<Product>(200, true, "Produto encontrado:", product);
+
+        return ResponseEntity.ok().body(response);
     }
 
 }
