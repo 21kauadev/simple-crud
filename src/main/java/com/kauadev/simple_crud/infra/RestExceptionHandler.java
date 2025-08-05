8a2c2fc8c84f07cffca98409634a1b2d2363d559
Kaua2123 <kauantnetsoares@gmail.com>
2025-08-05 10:26:59 -0300

    chore: add RestExceptionHandler controller advice to threat application errors

---
+++ src/main/java/com/kauadev/simple_crud/infra/RestExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.kauadev.simple_crud.infra;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.kauadev.simple_crud.domain.product.exceptions.ProductNotFoundException;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({ ProductNotFoundException.class })
+    private ResponseEntity<RestErrorMessage> productNotFoundHandler(ProductNotFoundException exception) {
+        RestErrorMessage threatedError = new RestErrorMessage(HttpStatus.NOT_FOUND.value(), exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(threatedError);
+    }
+
+}
