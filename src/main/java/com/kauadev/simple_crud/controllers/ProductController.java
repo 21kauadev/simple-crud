7bf0cfd5752824b38a42dc13de259ad1253e18ff
Kaua2123 <kauantnetsoares@gmail.com>
2025-08-05 10:46:20 -0300

    feat: finish basic CRUD in ProductController and ProductService

--- src/main/java/com/kauadev/simple_crud/controllers/ProductController.java
+++ src/main/java/com/kauadev/simple_crud/controllers/ProductController.java
@@ -5,12 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kauadev.simple_crud.domain.product.Product;
+import com.kauadev.simple_crud.domain.product.ProductDTO;
 import com.kauadev.simple_crud.infra.ApiResponse;
 import com.kauadev.simple_crud.services.ProductService;
 
@@ -35,6 +40,31 @@ public class ProductController {
     public ResponseEntity<ApiResponse<Product>> getProduct(@PathVariable("id") Long id) {
         Product product = productService.getProduct(id);
         ApiResponse<Product> response = new ApiResponse<Product>(200, true, "Produto encontrado:", product);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<ApiResponse<Product>> createProduct(@RequestBody ProductDTO data) {
+        Product product = productService.createProduct(data);
+        ApiResponse<Product> response = new ApiResponse<Product>(200, true, "Produto criado.", product);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<Product>> updateProduct(@PathVariable("id") Long id,
+            @RequestBody ProductDTO data) {
+        Product updatedProduct = productService.updateProduct(id, data);
+        ApiResponse<Product> response = new ApiResponse<Product>(200, true, "Produto atualizado", updatedProduct);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<Product>> deleteProduct(@PathVariable("id") Long id) {
+        productService.deleteProduct(id);
+        ApiResponse<Product> response = new ApiResponse<Product>(200, true, "Produto deletado", null);
 
         return ResponseEntity.ok().body(response);
     }
--- src/main/java/com/kauadev/simple_crud/services/ProductService.java
+++ src/main/java/com/kauadev/simple_crud/services/ProductService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kauadev.simple_crud.domain.product.Product;
+import com.kauadev.simple_crud.domain.product.ProductDTO;
 import com.kauadev.simple_crud.domain.product.exceptions.ProductNotFoundException;
 import com.kauadev.simple_crud.repositories.ProductRepository;
 
@@ -23,4 +24,32 @@ public class ProductService {
         return repository.findById(id).orElseThrow(ProductNotFoundException::new);
     }
 
+    public Product createProduct(ProductDTO data) {
+        Product product = new Product();
+
+        product.setName(data.name());
+        product.setPrice(data.price());
+        product.setQuantity(data.quantity());
+
+        return repository.save(product);
+    }
+
+    public Product updateProduct(Long id, ProductDTO data) {
+        Product product = repository.findById(id).orElseThrow(ProductNotFoundException::new);
+
+        // dessa vez alteramos os dados de um produto já criado e encontrado.
+        product.setName(data.name());
+        product.setPrice(data.price());
+        product.setQuantity(data.quantity());
+
+        // e o salvamos novamente.
+        return repository.save(product);
+    }
+
+    public Product deleteProduct(Long id) {
+        Product product = repository.findById(id).orElseThrow(ProductNotFoundException::new);
+        repository.delete(product);
+
+        return null;
+    }
 }
